5cc9acaebf459724c0d80599bfbf8f22a5b3b8bf
manug859 <manuel.agustin.gomez@mi.unc.edu.ar>
2024-12-09 02:18:27 -0300

    Agregar archivos de solución para el día 1 de 2024

---
+++ 2024/day1_1.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+
+#define N 1000
+
+void imprimir_arreglo(int a[], int tam) {
+    printf("[");
+    for (int i = 0; i < tam; i++) {
+        printf("%d", a[i]);
+        if (i < tam - 1) {
+            printf(", ");
+        }
+    }
+    printf("]\n");
+}
+
+int compare(const void* a, const void* b) {
+    return (*(int*)a - *(int*)b);
+}
+
+int modulo(int a, int b) {
+    int x = a - b;
+        if(x < 0) {
+            return -x;
+        }
+    return x;
+}
+
+int main(void) {
+    int i;
+    int j;
+    int k = 0;
+    int l_row[N];
+    int r_row[N];
+    int distancia = 0;
+
+
+    char lines[20];
+    FILE *file = fopen("input1_2024.txt", "r");
+        while(fgets(lines, sizeof(lines), file)) {
+            sscanf(lines, "%d  %d", &i, &j);
+            l_row[k] = i;
+            r_row[k] = j;
+            k++;
+        }
+        fclose(file);
+
+    qsort(l_row, N, sizeof(int), compare);
+    qsort(r_row, N, sizeof(int), compare);
+
+    for(int l = 0; l < N; l++) {
+        distancia += modulo(l_row[l], r_row[l]);
+    }
+        printf("distancia = %d\n", distancia);
+ 
+return 0;
+}
